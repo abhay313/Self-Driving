c36e8b52e0a515f1dce61892aa7aafca9cfe5736
Abhay Singh Yadav <43968252+abhay313@users.noreply.github.com>
2020-09-12 08:29:58 +0530

    tracking path for a vehicle

--- path_detection/tracking_path.py
+++ path_detection/tracking_path.py
@@ -2,8 +2,6 @@ import matplotlib.pylab as plt
 import cv2
 import numpy as np
 
-
-
 def region_of_interest(img, vertices):
     mask = np.zeros_like(img)
     match_mask_color = 255
@@ -11,10 +9,8 @@ def region_of_interest(img, vertices):
     masked_image = cv2.bitwise_and(img, mask)
     return masked_image
 
-
-
+# draw two lines which contains path
 def drow_the_lines(img, lines):
-
     for line in lines:
         for x1, y1, x2, y2 in line:
             cv2.line(img, (x1,y1), (x2,y2), (0, 255, 0), thickness=10)
@@ -24,20 +20,17 @@ def process(image):
     print(image.shape)
     height = image.shape[0]
     width = image.shape[1]
-
     region_of_interest_vertices = [(0, height),(width/2, height/2),(width, height)]
-
     gray_image = cv2.cvtColor(image, cv2.COLOR_RGB2GRAY)
     canny_image = cv2.Canny(gray_image, 100, 120)
     cropped_image = region_of_interest(canny_image,np.array([region_of_interest_vertices], np.int32),)
-
     lines = cv2.HoughLinesP(cropped_image,rho=2,theta=np.pi/180,threshold=50,lines=np.array([]),minLineLength=40,maxLineGap=100)
     image_with_lines = drow_the_lines(image, lines)
     return image_with_lines
 
+# capture testing video
 cap = cv2.VideoCapture('test.mp4')
-
-
+# extract frames of that video and modify it into our interested frames and then combine these frames into video
 
 while cap.isOpened():
     ret, frame = cap.read()
@@ -45,6 +38,6 @@ while cap.isOpened():
     cv2.imshow('frame', frame)
     if cv2.waitKey(1) & 0xFF == ord('q'):
         break
+        
 cap.release()
-
 cv2.destroyAllWindows()
